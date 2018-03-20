bddea61c5cf4be423a7620a21673be020cbf7aa3
AntonioKaranfiloski <36931861+AntonioKaranfiloski@users.noreply.github.com>
2018-03-20 13:49:57 +0100

    commit

--- app/src/main/java/com/example/anton/pmpdomasna3/Activity2.java
+++ app/src/main/java/com/example/anton/pmpdomasna3/Activity2.java
@@ -1,5 +1,6 @@
 package com.example.anton.pmpdomasna3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +10,16 @@ import android.widget.TextView;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
 public class Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+
     }
+
     public void lookup(View view){
         EditText the_word = (EditText) findViewById(R.id.editText_Search);
         String word = the_word.getText().toString();
@@ -24,6 +28,12 @@ public class Activity2 extends AppCompatActivity {
 
         TextView definition = (TextView) findViewById(R.id.textView_definition);
         definition.setText(defn);
+
+        Intent intent = new Intent();
+        intent.putExtra("TextValue", definition.getText().toString());
+        intent.setClass(Activity2.this, MainActivity.class);
+        startActivity(intent);
+
     }
 
     private String readDefinition(String word) {
@@ -39,4 +49,9 @@ public class Activity2 extends AppCompatActivity {
     }
         return null;
     }
-}
+
+        }
+
+   /* Intent intent = new Intent(Activity2.this, MainActivity.class);
+intent.putExtra("keyName", );  // pass your values and retrieve them in the other Activity using keyName
+    startActivity(intent); */
--- app/src/main/java/com/example/anton/pmpdomasna3/MainActivity.java
+++ app/src/main/java/com/example/anton/pmpdomasna3/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String s = getIntent().getStringExtra("TextValue");
+        TextView text = (TextView) findViewById(R.id.textview1);
+        text.setText(s);
+        //textview1.setText("value1: " + s + ");
     }
     public void OpenA (View view){
         startActivity(new Intent(this, Activity2.class));
